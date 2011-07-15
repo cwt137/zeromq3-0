ba0166fc0ab98335ecccfbaa8aa87ec945b00332
Pieter Hintjens <ph@imatix.com>
2011-07-15 07:12:20 +0200

    Fixed issue 230, SIGPIPE on OS/X

--- src/tcp_socket.cpp
+++ src/tcp_socket.cpp
@@ -158,6 +158,11 @@ int zmq::tcp_socket_t::open (fd_t fd_, int sndbuf_, int rcvbuf_)
         errno_assert (rc == 0);
     }
 
+#ifdef ZMQ_HAVE_OSX
+    int set = 1;
+    int rc = setsockopt (s, SOL_SOCKET, SO_NOSIGPIPE, &set, sizeof (int));
+    errno_assert (rc == 0);
+#endif
     return 0;
 }
 
